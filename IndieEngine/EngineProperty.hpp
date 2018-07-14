64ae336e43277f47ada7f4c350edb52eadb9629b
Snowapril <sinjihng@naver.com>
2018-07-14 23:54:23 +0900

    2018-07-14-Indent-modify

--- IndieEngine/EngineProperty.hpp
+++ IndieEngine/EngineProperty.hpp
@@ -6,16 +6,16 @@
 
 namespace Color {
 
-	const float White[4]			= { 1.0f,  1.0f,  1.0f,  1.0f };
-	const float Black[4]			= { 0.0f,  0.0f,  0.0f,  1.0f };
-	const float Red[4]				= { 1.0f,  0.0f,  0.0f,  1.0f };
-	const float Green[4]			= { 0.0f,  1.0f,  0.0f,  1.0f };
-	const float Blue[4]				= { 0.0f,  0.0f,  1.0f,  1.0f };
-	const float Yellow[4]			= { 1.0f,  1.0f,  0.0f,  1.0f };
-	const float Cyan[4]				= { 0.0f,  1.0f,  1.0f,  1.0f };
-	const float Magenta[4]			= { 1.0f,  0.0f,  1.0f,  1.0f };
-	const float Silver[4]			= { 0.75f, 0.75f, 0.75f, 1.0f };
-	const float LightSteelBlue[4]	= { 0.69f, 0.77f, 0.87f, 1.0f };
+	const float White[4]           = { 1.0f,  1.0f,  1.0f,  1.0f };
+	const float Black[4]           = { 0.0f,  0.0f,  0.0f,  1.0f };
+	const float Red[4]             = { 1.0f,  0.0f,  0.0f,  1.0f };
+	const float Green[4]           = { 0.0f,  1.0f,  0.0f,  1.0f };
+	const float Blue[4]            = { 0.0f,  0.0f,  1.0f,  1.0f };
+	const float Yellow[4]          = { 1.0f,  1.0f,  0.0f,  1.0f };
+	const float Cyan[4]            = { 0.0f,  1.0f,  1.0f,  1.0f };
+	const float Magenta[4]         = { 1.0f,  0.0f,  1.0f,  1.0f };
+	const float Silver[4]          = { 0.75f, 0.75f, 0.75f, 1.0f };
+	const float LightSteelBlue[4]  = { 0.69f, 0.77f, 0.87f, 1.0f };
 
 }
 
